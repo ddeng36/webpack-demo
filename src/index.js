e38472e7d7342e32bece17b002637175e65d3227
dengdingchao <dengdingchao@baidu.com>
2023-11-07 20:18:50 +0800

    代码分离-动态导入 dynamic import

--- src/index.js
+++ src/index.js
@@ -1,18 +1,11 @@
-import _ from 'lodash';
-import printMe from './print.js';
+import _ from "lodash";
+async function getComponent() {
+  const element = document.createElement("div");
+  const { default: _ } = await import("lodash");
+  element.innerHTML = _.join(["Hello", "webpack"], " ");
+  return element;
+}
 
- function component() {
-   const element = document.createElement('div');
-  const btn = document.createElement('button');
-
-   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
-
-  element.appendChild(btn);
-
-   return element;
- }
-
- document.body.appendChild(component());
+getComponent().then((component) => {
+  document.body.appendChild(component);
+});
