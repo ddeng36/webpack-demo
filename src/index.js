b8dc6ee7c6b3320bb47c81cc7982b041d63982c0
dengdingchao <dengdingchao@baidu.com>
2023-11-08 11:40:20 +0800

    lazy-load

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import _ from 'lodash';
-import { cube } from './math.js';
 
- function component() {
-  const element = document.createElement('pre');
+function component() {
+   const element = document.createElement('div');
+  const button = document.createElement('button');
+  const br = document.createElement('br');
 
-  element.innerHTML = [
-    'Hello webpack!',
-    '5 cubed is equal to ' + cube(5)
-  ].join('\n\n');
+  button.innerHTML = 'Click me and look at the console!';
+   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.appendChild(br);
+  element.appendChild(button);
+
+  // Note that because a network request is involved, some indication
+  // of loading would need to be shown in a production-level site/app.
+  button.onclick = e => import(/* webpackChunkName: "print" */ './print').then(module => {
+    const print = module.default;
+
+    print();
+  });
 
    return element;
  }
-
- document.body.appendChild(component());
+document.body.appendChild(component());
