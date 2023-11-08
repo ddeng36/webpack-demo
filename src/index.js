822e93c3fdc3a02400e58a344d0726257d5eba7c
dengdingchao <dengdingchao@baidu.com>
2023-11-08 11:13:41 +0800

    Hot Module Replacment

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
-import _ from "lodash";
-async function getComponent() {
-  const element = document.createElement("div");
-  const { default: _ } = await import("lodash");
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
+import _ from 'lodash';
+import printMe from './print.js';
+
+function component() {
+  const element = document.createElement('div');
+  const btn = document.createElement('button');
+
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
+
   return element;
 }
 
-getComponent().then((component) => {
-  document.body.appendChild(component);
-});
+// document.body.appendChild(component());
+let element = component(); // 存储 element，以在 print.js 修改时重新渲染
+document.body.appendChild(element);
+
+ if (module.hot) {
+   module.hot.accept('./print.js', function() {
+     console.log('111111111!');
+    // printMe();
+    document.body.removeChild(element);
+    element = component(); // 重新渲染 "component"，以便更新 click 事件处理函数
+    document.body.appendChild(element);
+   })
+ }
--- webpack.config.js
+++ webpack.config.js
@@ -2,26 +2,22 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+     app: './src/index.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+   hot: true,
+  },
   plugins: [
     new HtmlWebpackPlugin({
-     title: 'Caching',
+      title: 'Hot Module Replacement',
     }),
   ],
   output: {
-   filename: '[name].[contenthash].js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-  },   optimization: {
-    runtimeChunk: 'single',
-    splitChunks: {
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          chunks: 'all',
-        },
-      },
-    },
   },
 };
